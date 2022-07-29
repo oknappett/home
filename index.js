b96d1a372ecfadaf888de6a16b93a961ee889614
oknappett <oknappett@gmail.com>
2022-07-29 12:33:15 +0100

    Add highliting to nav bar

--- index.js
+++ index.js
@@ -28,3 +28,21 @@ email.addEventListener('click', () => {
 insta.addEventListener('click', () => {
     window.open(igLink, '_blank').focus()
 })
+
+let sections = document.querySelectorAll('section');
+let menu = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach((i) =>{
+        let id  = i.getAttribute('id');
+        let top = window.scrollY;
+        let offset = i.offsetTop - 150;
+        let height = i.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            menu.forEach((link) => {
+                link.classList.remove('active');
+                document.querySelector("header nav a[href*=" + id + "]").classList.add("active");            });
+        }
+    });
+};                      
