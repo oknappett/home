7362990872ac755bbcc1fa0b09401e61e93899f3
Ollie Knappett <oknappett@gmail.com>
2022-08-01 20:46:37 +0000

    modified nav bar and the padding between divs

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ let sections = document.querySelectorAll("section");
 let menu = document.querySelectorAll("#navbar nav a");
 
 window.onscroll = () =>{
-    sections.forEach((i) =>{
+    sections.forEach((i =>{
         let id  = i.getAttribute('id');
         let top = window.scrollY;
         let offset = i.offsetTop - 150;
@@ -47,12 +47,11 @@ window.onscroll = () =>{
                 link.classList.remove('active');
                 document.querySelector("nav a[href*=" + id + "]").classList.add("active");            });
         }
-    });
+    }));
     stickynavbar()
 };                      
 
 var navbar = document.getElementById('navbar');
-var homeButton= document.getElementById('home-button');
 var sticky = navbar.offsetTop;
 function stickynavbar(){
     if(window.pageYOffset >=sticky){
